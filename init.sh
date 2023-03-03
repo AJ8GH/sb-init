be7695d66522ca160ee51ee97498881275d25eb1
Adam Jonas <adamjonas25@gmail.com>
2023-03-03 22:21:30 +0000

    Link with github api

--- init.sh
+++ init.sh
@@ -3,13 +3,13 @@
 name=$1
 package=$2
 project_dir="/Users/adamjonas/Projects/$name"
+DIR="$(dirname "$(readlink -f "$0")")"
+token=$(cat "$DIR/.token")
 
 cp -R "/Users/adamjonas/Projects/spring-boot-skeleton" "$project_dir"
 cd "$project_dir"
 
 rm -rf .git **/*.iml .idea logs **/logs target **/target
-
-git init
 
 mv "./application/src/main/java/io/github/aj8gh/skeleton" "./application/src/main/java/io/github/aj8gh/$package"
 mv "./application/src/test/java/io/github/aj8gh/skeleton" "./application/src/test/java/io/github/aj8gh/$package"
@@ -24,5 +24,20 @@ sed -i '' "s/spring-boot-skeleton/$name/g" ./coverage/pom.xml
 sed -i '' "s/>skeleton</>$package</g" ./pom.xml
 sed -i '' "s/skeleton/$package/g" "$project_dir/application/src/main/java/io/github/aj8gh/$package/Application.java"
 sed -i '' "s/skeleton/$package/g" "$project_dir/application/src/test/java/io/github/aj8gh/$package/ApplicationTest.java"
+
+git init
+git add .
+git commit -m 'Initial commit'
+git remote add origin "git@github.com:AJ8GH/$name.git"
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $token"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/user/repos \
+  -d "{\"name\":\"$name\"}"
+
+git push -u origin main
 
 idea "$project_dir"
